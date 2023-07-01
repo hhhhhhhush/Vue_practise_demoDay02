26954ba25618981331bf02926eb0c86b18087472
mzz <2831376148@qq.com>
2023-07-01 09:39:57 +0800

    one

---
+++ src/components/MyGoods.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="son">
+      <h3>我是子组件</h3>
+      <h4>商品名称:隆江猪脚饭</h4>
+      <h4>商品价格:</h4>
+      <button>点我砍一刀</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'model',
+
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+
+<style scoped>
+    .son{
+        border: 1px solid red;
+    }
+</style>
