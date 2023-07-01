d71a7198d2f1cbb98fbd1fb93c95d6175c5f81b3
mzz <2831376148@qq.com>
2023-07-01 10:50:16 +0800

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <h1>我是根组件</h1>
+    <input type="text" v-model="msg">
+    <input type="text" :value="msg" @input="changeVal($event)">
+    <!-- 
+      v-model的本质是语法糖：两个语法做的事情是一样的，只是写法不同
+      父传子: props ==> :value
+      子传父：@input
+     -->
+    <my-goods :value="price" @input="price=$event"></my-goods>
+    <my-goods v-model="price"></my-goods>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import MyGoods from './components/MyGoods.vue'
+export default {
+  components: {
+    MyGoods
+  },
+  data() {
+    return {
+      msg: "",
+      price: 100
+    }
+  },
+  methods: {
+    changeVal (e) {
+      this.msg = e.target.value;
+    }
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/MyGoods.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="son">
+        <h3>我是子组件</h3>
+        <h4>商品名称:隆江猪脚饭</h4>
+        <h4>商品价格: {{ value }}</h4>
+        <button @click="$emit('input',value-1)">点我砍一刀</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyGoods',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    props: {
+        value: Number
+    }
+}
+</script>
+
+<style scoped>
+.son {
+    border: 1px solid red;
+}
+</style>
